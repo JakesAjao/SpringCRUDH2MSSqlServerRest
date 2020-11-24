102cb98ae1bc2f62c494e2957782f5e5c91a7eda
Jacob Ajao <jacob.ajao@fcmb.com>
2020-11-24 13:15:29 +0100

    Initial Commit

---
+++ src/main/java/com/fcmb/SpringCRUDHibernateMSSqlServerRest/SpringCrudHibernateMsSqlServerRestApplication.java
@@ -0,0 +1,13 @@
+package com.fcmb.SpringCRUDHibernateMSSqlServerRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudHibernateMsSqlServerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudHibernateMsSqlServerRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcmb/SpringCRUDHibernateMSSqlServerRest/SpringCrudHibernateMsSqlServerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcmb.SpringCRUDHibernateMSSqlServerRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudHibernateMsSqlServerRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
